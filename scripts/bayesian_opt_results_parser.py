12f2ea4a93876aadc41fb80c207493a5ebcf3c3d
Sébastien Iooss <13920346+Net-Mist@users.noreply.github.com>
2020-10-07 16:36:44 +0200

    type 0 (#4)
    * improve script for results visu
    
    * Define type0
    
    Co-authored-by: Sebastien Iooss <sebastien.iooss@upstride.io>

--- scripts/bayesian_opt_results_parser.py
+++ scripts/bayesian_opt_results_parser.py
@@ -9,13 +9,16 @@ import upstride_argparse as argparse
 arguments = [
     [str, "server", '', 'address of the server to connect using ssh'],
     [str, 'remote_dir', '', "directory of the keras tuner experiment on the remote server"],
-    [str, 'local_dir', '', 'path of the directory containing the results']
+    [str, 'csv_path', '/tmp/results.csv', 'path to write csv file'],
+    [bool, 'no_plot', False, 'if true then don\'t plot the results']
 ]
 
+plot = True
 try:
   matplotlib.use("GTK3Agg")
 except ImportError:
-  pass
+  print("can't load matplotlib")
+  plot = False
 sns.set(style="darkgrid")
 
 
@@ -44,19 +47,43 @@ def split_json(cmd_out):
   return jsons
 
 
+def parse_str(e):
+  if e is None:
+    return '0'
+  return str(e)
+
+
 def main():
+  global plot
   args = argparse.parse_cmd(arguments)
+  if args['no_plot']:
+    plot = False
   server = args['server']
   remote_dir = args['remote_dir']
   out = run_bash(f'ssh {server} "cd {remote_dir} && cat */trial.json"')
   jsons = split_json(out)
+  csv_content = 'experiment_id,factor,framework,depth,score\n'
   for trial in jsons:
     trial = json.loads(trial)
-    print()
-    print(trial['trial_id'])
-    print(trial['hyperparameters']['values'])
-    print(trial['score'])
+    values = trial['hyperparameters']['values']
+    csv_values = [trial['trial_id'], values['factor'], values['framework'], values['depth'], trial['score']]
+    csv_content += ','.join(list(map(parse_str, csv_values))) + '\n'
 
+  with open(args['csv_path'], 'w') as f:
+    f.write(csv_content)
+
+  if not plot:
+    return
+
+  # plot the results
+  data = pd.read_csv(args['csv_path'])
+  print(data)
+  f, ax = plt.subplots(figsize=(9, 6))
+  data = data.drop_duplicates(subset=["factor", "depth"])
+  data = data.pivot("factor", "depth", "score")
+  print(data)
+  sns.heatmap(data, annot=True, vmin=0, vmax=1, cmap='CMRmap')
+  plt.show()
 
 
 if __name__ == "__main__":
