78e918fac4522a8dacf095fcb2ab3bad3856e638
Rifat <rifat.arefin@upstride.io>
2020-10-14 21:04:32 +0200

    unit test: gumbel softmax and split weights merged  with commit wip, bug fixes ...

--- src/models/test_fbnetv2.py
+++ src/models/test_fbnetv2.py
@@ -5,6 +5,7 @@ import yaml
 import tensorflow as tf
 import numpy as np
 from . import fbnetv2
+import scipy.stats as ss
 
 
 class TestBinaryVector(unittest.TestCase):
@@ -112,3 +113,82 @@ class TestPostTrainingAnalysis(unittest.TestCase):
       read = yaml.safe_load(f)
     self.assertDictEqual({"toto_1": 1, "toto_2": 8}, read)
     shutil.rmtree(tmpdir)
+
+class TestSplitTtrainableWeights(unittest.TestCase):
+  def test_split_trainable_weights(self):
+    layer0 = tf.keras.layers.Input((32, 32, 3))
+    layer1 = tf.keras.layers.Conv2D(8, kernel_size=3, strides=1, padding='same')
+    layer2 = fbnetv2.ChannelMasking(2, 8, 2, 'abc', gumble_noise=False)
+    model = tf.keras.Sequential([layer0, layer1, layer2])
+
+    weights, arch_params = fbnetv2.split_trainable_weights(model)
+
+    true_total_weight_param = 3*3*3*8+8
+    true_total_arch_param = len(range(2, 8+1, 2))
+
+    # calculate number of weight params returned by  the function
+    total_weight_params = 0
+    for w in weights:
+      total_weight_params += np.prod(w.shape.as_list())
+
+    # calculate number of architecture params returned by  the function
+    total_arch_params = 0
+    for p in arch_params:
+      total_arch_params += np.prod(p.shape.as_list())
+
+    self.assertEqual(total_arch_params, true_total_arch_param)
+    self.assertEqual(total_weight_params, true_total_weight_param)
+    self.assertEqual(total_weight_params+total_arch_params, true_total_weight_param+true_total_arch_param)
+
+  def test_not_arch_params(self):
+    layer0 = tf.keras.layers.Input((32, 32, 3))
+    layer1 = tf.keras.layers.Conv2D(8, kernel_size=3, strides=1, padding='same')
+    model = tf.keras.Sequential([layer0, layer1])
+
+    # check if it raises error when there is no architectural parameters by the name 'alpha'
+    self.assertRaises(ValueError, fbnetv2.split_trainable_weights, model, arch_params_name='alpha')
+
+class TestGumbelSoftmax(unittest.TestCase):
+  def testSampling(self):
+    fbnetv2.define_temperature(5.)
+    noise = 0.0001
+    logits = tf.constant([-1., 0.5, 1.])
+
+    g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
+
+    self.assertEqual(logits.shape.as_list(), g.shape.as_list())
+    self.assertEqual(g.numpy().mean(), 1.0)
+    self.assertSequenceEqual(g.numpy().tolist(), tf.math.softmax((logits+noise)/5.).numpy().tolist())
+
+  def testUniformLikeDist(self):
+    # set temperature values to high to see Uniform like distribution
+    fbnetv2.define_temperature(5.0)
+    logits = tf.constant([-2., 2., -2.5, -2.])
+
+    g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
+
+    # Test the distribution using Kolmogorove-Smirnov test as 
+    # described here (https://stackoverflow.com/questions/22392562/how-can-check-the-distribution-of-a-variable-in-python)
+
+    _, p = ss.kstest(g.numpy(), ss.uniform.cdf)
+
+    th = 0.05
+
+    # Check the significance, if the signifncace are higher than threshold(th), we can assume it to be uniform
+    self.assertGreater(p, th)
+
+  def testOnehotLikeDist(self):
+    # set temperature values to high to see Onehot like distribution
+    fbnetv2.define_temperature(0.00001)
+    logits = tf.constant([-2., 2., -2.5, -2.])
+
+    g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
+
+    _, p = ss.kstest(g.numpy(), ss.uniform.cdf)
+
+    th = 0.05
+
+    # Check the significance, if the signifncace are lesser than threshold(th), 
+    # we can assume it to be not uniform thus onhot
+    self.assertLess(p, th)
+  
