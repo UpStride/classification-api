eed597d3b7b0bea1a31f66f92741cb4dc5963ea4
Bharath Sridharan <bharath.sridharna@upstride.io>
2020-10-15 16:01:45 +0530

    test_losses unit test fixed

--- src/test_losses.py
+++ src/test_losses.py
@@ -7,6 +7,7 @@ from src.losses import flops_loss
 class TestLosses(unittest.TestCase):
   def test_flops_loss(self):
     model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(24, 24, 3)),
         tf.keras.layers.Conv2D(3, (3, 3), padding='same', use_bias=False),
         ChannelMasking(1, 3, 1, "hello", gumble_noise=False)
     ])
@@ -31,6 +32,7 @@ class TestLosses(unittest.TestCase):
 
   def test_flops_with_intermediate_ops_loss(self):
     model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(24, 24, 3)),
         tf.keras.layers.Conv2D(3, (3, 3), padding='same', use_bias=False),
         tf.keras.layers.ReLU(),
         ChannelMasking(1, 3, 1, "hello", gumble_noise=False)
