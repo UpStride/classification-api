ebe03a1fe920e31f2638af2e8212448f2c70ed24
Sebastien Iooss <sebastien.iooss@upstride.io>
2020-10-15 11:58:13 +0200

    correct unit-test

--- src/models/test_fbnetv2.py
+++ src/models/test_fbnetv2.py
@@ -1,11 +1,10 @@
 import unittest
-import shutil 
+import shutil
 import tempfile
 import yaml
 import tensorflow as tf
 import numpy as np
 from . import fbnetv2
-import scipy.stats as ss
 
 
 class TestBinaryVector(unittest.TestCase):
@@ -30,11 +29,12 @@ class TestGetMask(unittest.TestCase):
     self.assertEqual(mask.dtype, tf.float32)
     self.assertTrue(np.array_equal(mask, [10.,  8.,  5.,  5.]))
 
+
 class TestChannelMasking(unittest.TestCase):
   def test_init(self):
     cm = fbnetv2.ChannelMasking(1, 5, 2, 'toto')
     self.assertEqual(cm.channel_sizes, [1, 3, 5])
-    
+
   def test_build_manual(self):
     cm = fbnetv2.ChannelMasking(1, 5, 2, 'toto')
     cm.build((15, 15, 3))
@@ -46,7 +46,7 @@ class TestChannelMasking(unittest.TestCase):
   def test_build_keras(self):
     cm = fbnetv2.ChannelMasking(1, 5, 2, 'toto', gumble_noise=False)
     model = tf.keras.Sequential([tf.keras.layers.Conv2D(5, (3, 3), padding='same', use_bias=False), cm])
-    model(tf.zeros((1, 24, 24, 3), dtype=tf.float32)) # build is called here
+    model(tf.zeros((1, 24, 24, 3), dtype=tf.float32))  # build is called here
     self.assertTrue(np.array_equal(cm.alpha.numpy(), [1., 1., 1.]))
     self.assertTrue(np.array_equal(cm.binary_vectors[0].numpy(), [1., 0., 0., 0., 0.]))
     self.assertTrue(np.array_equal(cm.binary_vectors[1].numpy(), [1., 1., 1., 0., 0.]))
@@ -55,7 +55,7 @@ class TestChannelMasking(unittest.TestCase):
   def test_call(self):
     cm = fbnetv2.ChannelMasking(1, 5, 2, 'toto', gumble_noise=False)
     model = tf.keras.Sequential([cm])
-    out = model(tf.ones((1, 3, 3, 5), dtype=tf.float32)) # build is called here
+    out = model(tf.ones((1, 3, 3, 5), dtype=tf.float32))  # build is called here
     # check g parameter
     for e in cm.g.numpy():
       self.assertAlmostEqual(e, 1/3)
@@ -67,7 +67,8 @@ class TestChannelMasking(unittest.TestCase):
     self.assertAlmostEqual(components.numpy()[2], 2/3)
     self.assertAlmostEqual(components.numpy()[3], 1/3)
     self.assertAlmostEqual(components.numpy()[4], 1/3)
-    
+
+
 class TestExponentialDecay(unittest.TestCase):
   def non_increasing(self, decay):
     """Checks if all the function provided are non increasing over a range
@@ -79,14 +80,14 @@ class TestExponentialDecay(unittest.TestCase):
         bool : Compares ith and i + 1st element of the value_list
         returns True if all i >= i+1 else False
     """
-    value_list = [decay(i) for i in range(1,100)]
+    value_list = [decay(i) for i in range(1, 100)]
     return all([i >= j for i, j in zip(value_list, value_list[1:])])
 
   def test_exponential_decay(self):
     decay = fbnetv2.exponential_decay(5, 1, 0.956)
 
-    self.assertEqual(decay(0), 5)  
-    self.assertAlmostEqual(decay(10), 3.188,places=3)
+    self.assertEqual(decay(0), 5)
+    self.assertAlmostEqual(decay(10), 3.188, places=3)
 
     # test function is not increasing over number of epochs
     self.assertTrue(self.non_increasing(decay), True)
@@ -95,24 +96,26 @@ class TestExponentialDecay(unittest.TestCase):
     decay = fbnetv2.exponential_decay(5, 1, 1.1)
     self.assertFalse(self.non_increasing(decay), False)
 
+
 class TestPostTrainingAnalysis(unittest.TestCase):
   def test_post_training_anaysis(self):
     cm1 = fbnetv2.ChannelMasking(1, 5, 2, 'toto_1_savable', gumble_noise=False)
     cm2 = fbnetv2.ChannelMasking(8, 16, 4, 'toto_2_savable', gumble_noise=False)
     model = tf.keras.Sequential(
-      [tf.keras.layers.Conv2D(5, (3, 3), padding='same', use_bias=False), 
-      cm1,
-      tf.keras.layers.Conv2D(16, (3, 3), padding='same', use_bias=False), 
-      cm2,
-      ])
-    model(tf.zeros((1, 24, 24, 3), dtype=tf.float32)) # build is called here
-    tmpdir = tempfile.mkdtemp() 
+        [tf.keras.layers.Conv2D(5, (3, 3), padding='same', use_bias=False),
+         cm1,
+         tf.keras.layers.Conv2D(16, (3, 3), padding='same', use_bias=False),
+         cm2,
+         ])
+    model(tf.zeros((1, 24, 24, 3), dtype=tf.float32))  # build is called here
+    tmpdir = tempfile.mkdtemp()
     tmpfile = tmpdir + "/test.yaml"
-    fbnetv2.post_training_analysis(model,tmpfile)
+    fbnetv2.post_training_analysis(model, tmpfile)
     with open(tmpfile, 'r') as f:
       read = yaml.safe_load(f)
     self.assertDictEqual({"toto_1": 1, "toto_2": 8}, read)
     shutil.rmtree(tmpdir)
+
 
 class TestSplitTtrainableWeights(unittest.TestCase):
   def test_split_trainable_weights(self):
@@ -148,6 +151,7 @@ class TestSplitTtrainableWeights(unittest.TestCase):
     # check if it raises error when there is no architectural parameters by the name 'alpha'
     self.assertRaises(ValueError, fbnetv2.split_trainable_weights, model, arch_params_name='alpha')
 
+
 class TestGumbelSoftmax(unittest.TestCase):
   def testSampling(self):
     fbnetv2.define_temperature(5.)
@@ -157,25 +161,28 @@ class TestGumbelSoftmax(unittest.TestCase):
     g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
 
     self.assertEqual(logits.shape.as_list(), g.shape.as_list())
-    self.assertEqual(g.numpy().mean(), 1.0)
-    self.assertSequenceEqual(g.numpy().tolist(), tf.math.softmax((logits+noise)/5.).numpy().tolist())
+    self.assertAlmostEqual(g.numpy().sum(), 1.0, 6)
+    self.assertEqual(g.numpy().tolist(), tf.math.softmax((logits+noise)/5.).numpy().tolist())
 
   def testUniformLikeDist(self):
     # set temperature values to high to see Uniform like distribution
-    fbnetv2.define_temperature(5.0)
+    fbnetv2.define_temperature(500000.0)
     logits = tf.constant([-2., 2., -2.5, -2.])
 
     g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
 
-    # Test the distribution using Kolmogorove-Smirnov test as 
-    # described here (https://stackoverflow.com/questions/22392562/how-can-check-the-distribution-of-a-variable-in-python)
+    for i in range(4):
+      self.assertAlmostEqual(float(g[i].numpy()), 0.25, 5)
 
-    _, p = ss.kstest(g.numpy(), ss.uniform.cdf)
-
-    th = 0.05
-
-    # Check the significance, if the signifncace are higher than threshold(th), we can assume it to be uniform
-    self.assertGreater(p, th)
+    previous_g = g
+    for t in range(100, 1, -1):
+      fbnetv2.define_temperature(t)
+      g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
+      for i in [0, 2, 3]:
+        self.assertLess(g[i], previous_g[i])
+      self.assertLess(previous_g[1], g[1])
+      previous_g = g
+    fbnetv2.define_temperature(5.0)
 
   def testOnehotLikeDist(self):
     # set temperature values to high to see Onehot like distribution
@@ -184,11 +191,8 @@ class TestGumbelSoftmax(unittest.TestCase):
 
     g = fbnetv2.gumbel_softmax(logits, gumble_noise=False)
 
-    _, p = ss.kstest(g.numpy(), ss.uniform.cdf)
-
-    th = 0.05
-
-    # Check the significance, if the signifncace are lesser than threshold(th), 
-    # we can assume it to be not uniform thus onhot
-    self.assertLess(p, th)
-  
+    self.assertAlmostEqual(g[0], 0.)
+    self.assertAlmostEqual(g[1], 1.)
+    self.assertAlmostEqual(g[2], 0.)
+    self.assertAlmostEqual(g[3], 0.)
+    fbnetv2.define_temperature(5.0)
