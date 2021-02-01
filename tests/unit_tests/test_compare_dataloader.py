d7ecb3a6f94334fc320d4b26ab82567ad92e2a77
Sebastien Iooss <archimist.linux@gmail.com>
2021-02-01 12:06:17 +0100

    code review and comments

--- tests/unit_tests/test_compare_dataloader.py
+++ tests/unit_tests/test_compare_dataloader.py
@@ -1,10 +1,30 @@
 """ 
+TODO : define testing procedure
+
 Known differences: 
 
 1) Our augmentation strategy works by taking mean by stacking each color channel whereas
 DCN takes each pixel wise mean
 
-2) There is very small difference in the mean between Keras Transate and our version.
+Algorithmically speaking, we do:
+Input: images, r_mean, g_mean, b_mean
+Alg: 
+  - substract r_mean from images[:, :, :, 0] (dimensions are bs, h, w, c)
+  - substract g_mean from images[:, :, :, 1]
+  - substract b_mean from images[:, :, :, 2]
+  - normalize
+
+They do:
+Input: images, mean
+Alg: 
+  - substract mean from images[:, :, :, :] (dimensions are bs, h, w, c)
+  - normalize
+
+note: their way of doing thing is unexpected so we will not implement it in classification-api
+but we will use there way here for testing purposes
+
+2) There is very small difference in the output between Keras Transate and our version.
+TODO : visual check
 
 """
 import unittest
@@ -17,18 +37,18 @@ import cv2
 
 import tensorflow as tf 
 
-from tfrecord_writer import build_tfrecord_dataset
+from scripts.tfrecord_writer import build_tfrecord_dataset
 from src.data.dataloader import get_dataset
-from src.data.augmentations import Normalize, Translate
 
 class TestCompareDataLoader(unittest.TestCase):
   @classmethod
   def setUp(self):
-    self.image_ones = np.ones((20, 224, 224, 3), dtype=np.uint8) * 255
+    self.image_ones = np.ones((20, 224, 224, 3), dtype=np.uint8) * 255 # TODO rename image_white
     np.random.seed(42)
-    self.image_random = np.random.uniform(low=0.0, high=255., size=(20, 224, 224, 3))
+    self.image_random = np.random.uniform(low=0.0, high=255., size=(20, 224, 224, 3)) # TODO size should be a global const variable
 
   def get_dcn_dataloader(self, image_array):
+    # TODO add a link to the file
     """DCN data loader from the DCN training file. takes input as a image array, normalizes the input and returns it
     Note: some parts are commented as its not required for this specific tests. 
     Args:
@@ -38,6 +58,7 @@ class TestCompareDataLoader(unittest.TestCase):
         [numpy array]: Normalized output
     """
     # (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    # TODO remove y_train
     (X_train, y_train) = image_array, np.ones((20, 2)) 
     nb_classes                           = 2
     n_train                              = 20
@@ -68,6 +89,7 @@ class TestCompareDataLoader(unittest.TestCase):
 
     return X_train # Y_train, X_val, Y_val
 
+  # TODO move this function out of this class
   def create_fake_dataset_and_convert_to_tfrecords(self, image_array, n_images_per_class=2):
     dataset_dir = tempfile.mkdtemp()
     os.makedirs(os.path.join(dataset_dir, 'cat'), exist_ok=True)
@@ -109,11 +131,12 @@ class TestCompareDataLoader(unittest.TestCase):
       subtract_mean = tf.reduce_mean(normalize, axis=0)
       X_train_from_our_dataloader = normalize - subtract_mean
 
-    
+    # TODO remove print function or add more details
     print(np.min(X_train_from_our_dataloader), np.max(X_train_from_our_dataloader))
     X_train_from_dcn_dataloader = self.get_dcn_dataloader(self.image_ones)
     print(np.min(X_train_from_dcn_dataloader), np.max(X_train_from_dcn_dataloader))
 
+    # TODO X should be x
     self.assertEqual(np.allclose(X_train_from_our_dataloader, X_train_from_dcn_dataloader), True)
 
     # clean up
@@ -123,9 +146,12 @@ class TestCompareDataLoader(unittest.TestCase):
     # create fake dataset and convert TFrecords
     dataset_dir = self.create_fake_dataset_and_convert_to_tfrecords(image_array=self.image_random, n_images_per_class=10)
 
+    # TODO move this comment at the top of the file
     # Our augmentation strategy works by taking mean by stacking each color channel whereas DCN takes each pixel wise mean 
     # Not sure if this difference can cause changes to the final accuracy. 
 
+    # TODO explain or remove
+    # explain : why you wrote this code, why you commented id and why you didn't remove it
     # image_normalized = self.image_random / 255.
     # r = np.dstack([image_normalized[i][:, :, 0] for i in range(len(image_normalized))])
     # g = np.dstack([image_normalized[i][:, :, 1] for i in range(len(image_normalized))])
@@ -138,16 +164,11 @@ class TestCompareDataLoader(unittest.TestCase):
       'data_dir': dataset_dir,
       'batch_size': 20,
       'train_split_id': 'train',
-      # 'Normalize': {'only_subtract_mean': True,
-      # 'scale_in_zero_to_one': True,
-      # 'mean': mean_data, 
-      # 'std': [0, 0, 0]
-      # }
     }
     
     # get data from tfrecord
     dataset = get_dataset(config, [], 2, 'train')
-    for image, _  in dataset:
+    for image, _  in dataset: # TODO dataset.__next__()
       # Normalize the image similar to DCN strategy
       normalize = image / 255.
       subtract_mean = tf.reduce_mean(normalize, axis=0)
@@ -161,10 +182,12 @@ class TestCompareDataLoader(unittest.TestCase):
     # clean up
     shutil.rmtree(dataset_dir)
 
+  # TODO update name
   def test_compare_with_translate_random(self):
     # create fake dataset
     dataset_dir = self.create_fake_dataset_and_convert_to_tfrecords(image_array=self.image_random, n_images_per_class=10)
 
+    # TODO should be removed
     image_normalized = self.image_random / 255.
     r = np.dstack([image_normalized[i][:, :, 0] for i in range(len(image_normalized))])
     g = np.dstack([image_normalized[i][:, :, 1] for i in range(len(image_normalized))])
@@ -184,6 +207,7 @@ class TestCompareDataLoader(unittest.TestCase):
     }
     
     dataset = get_dataset(config, ['Translate', 'RandomHorizontalFlip'], 2, 'train')
+    # TODO same issue for loop
     for image, _  in dataset:
       # Normalize the image similar to DCN strategy
       normalize = image / 255.
@@ -212,3 +236,5 @@ class TestCompareDataLoader(unittest.TestCase):
 
     # clean up
     shutil.rmtree(dataset_dir)
+
+# TODO one more test : do the data augmentation 1000 fimes, compare the mean of one pixel
