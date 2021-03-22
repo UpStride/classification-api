ddf3e95cf21b81e326aac6cd244b21073e752743
Bharath Sridharan <bharath.sridharan@upstride.io>
2021-03-22 08:58:19 +0100

    updated review comments and fixed resnet issue

--- tests/unit_tests/test_channels_first_last.py
+++ tests/unit_tests/test_channels_first_last.py
@@ -2,11 +2,12 @@ import unittest
 
 import tensorflow as tf
 from src.models import model_name_to_class
+from tqdm import tqdm
 
 class TestCompareChannelsFirstLast(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
-    cls.kwargs = {
+    cls.model_kwargs = {
       'input_size': [224, 224, 3],
       'changing_ids': [],
       'num_classes': 10,
@@ -16,8 +17,8 @@ class TestCompareChannelsFirstLast(unittest.TestCase):
 
     # below model do not work for upstride types
     remove_models = [
-      # The channel_last and first values mismatch only for upstride types. 
-      # FIXME when this is prioritized.
+      # These models do not match EfficientDet definition. There is a open Pull request yet to be merged. 
+      # This should be included once the pull request is merged.
       "EfficientNetB0", 
       "EfficientNetB1",
       "EfficientNetB2",
@@ -34,42 +35,50 @@ class TestCompareChannelsFirstLast(unittest.TestCase):
 
     tmp_list_models = model_name_to_class
     # remove models that are not supported
-    [tmp_list_models.pop(i) for i in remove_models]
+    [tmp_list_models.pop(model) for model in remove_models]
     cls.list_models_upstride = tmp_list_models.values()
 
   def test_compare_model_params_tensorflow(self):
-    self.kwargs.update({"upstride_type": -1, "factor": 1}) 
-    for model1, model2 in zip(self.list_of_models, self.list_of_models):
-      if model1 == model2: # would be same, just a safety check
-        # switch to channels first
-        tf.keras.backend.set_image_data_format('channels_first')
-        model_NCHW = model1(**self.kwargs).build()
-        model_NCHW_params = model_NCHW.count_params()
-        # switch back to channels last
-        tf.keras.backend.set_image_data_format('channels_last')
-        model_NHWC = model2(**self.kwargs).build()
-        model_NHWC_params = model_NHWC.count_params()
-        # compare
-        print(f"Model Name    : {model1.__name__} TensorFlow")
-        print(f"Channels_last : {model_NHWC_params:,}")
-        print(f"Channels_first: {model_NCHW_params:,}") 
-        self.assertEqual(model_NHWC_params, model_NCHW_params)
+    self.model_kwargs.update({"upstride_type": -1, "factor": 1})
+    print("Building models for Channels_first and Channels_last for Tensorflow and Compare") 
+    for model in tqdm(self.list_of_models):
+      # switch to channels first
+      tf.keras.backend.set_image_data_format('channels_first')
+      model_NCHW = model(**self.model_kwargs).build()
+      model_NCHW_params = model_NCHW.count_params()
+      del model_NCHW
+      tf.keras.backend.clear_session()
+      # switch back to channels last
+      tf.keras.backend.set_image_data_format('channels_last')
+      model_NHWC = model(**self.model_kwargs).build()
+      model_NHWC_params = model_NHWC.count_params()
+      del model_NHWC
+      tf.keras.backend.clear_session()
+      # compare
+      # print(f"Model Name    : {model.__name__} TensorFlow")
+      # print(f"Channels_last : {model_NHWC_params:,}")
+      # print(f"Channels_first: {model_NCHW_params:,}") 
+      self.assertEqual(model_NHWC_params, model_NCHW_params)
 
   def test_compare_model_params_upstride(self):
-    for up_type in [2]: #TODO add type1 later. resnet model parameters do not match. FIXME when this is prioritized.
-      self.kwargs.update({"upstride_type": up_type, "factor": 2**up_type}) 
-      for model1, model2 in zip(self.list_models_upstride, self.list_models_upstride):
-        if model1 == model2: # would be same, just a safety check
-          # switch to channels first
-          tf.keras.backend.set_image_data_format('channels_first')
-          model_NCHW = model1(**self.kwargs).build()
-          model_NCHW_params = model_NCHW.count_params()
-          # switch back to channels last
-          tf.keras.backend.set_image_data_format('channels_last')
-          model_NHWC = model2(**self.kwargs).build()
-          model_NHWC_params = model_NHWC.count_params()
-          # compare
-          print(f"Model Name    : {model1.__name__} UpStride type{up_type}")
-          print(f"Channels_last : {model_NHWC_params:,}")
-          print(f"Channels_first: {model_NCHW_params:,}") 
-          self.assertEqual(model_NHWC_params, model_NCHW_params)
+    for up_type in [1, 2]: 
+      self.model_kwargs.update({"upstride_type": up_type, "factor": 2**up_type}) 
+      print(f"Building models for Channels_first and Channels_last for Upstride type{up_type} and Compare") 
+      for model in tqdm(self.list_models_upstride):
+        # switch to channels first
+        tf.keras.backend.set_image_data_format('channels_first')
+        model_NCHW = model(**self.model_kwargs).build()
+        model_NCHW_params = model_NCHW.count_params()
+        del model_NCHW
+        tf.keras.backend.clear_session()
+        # switch back to channels last
+        tf.keras.backend.set_image_data_format('channels_last')
+        model_NHWC = model(**self.model_kwargs).build()
+        model_NHWC_params = model_NHWC.count_params()
+        del model_NHWC
+        tf.keras.backend.clear_session()
+        # compare
+        # print(f"Model Name    : {model.__name__} UpStride type{up_type}")
+        # print(f"Channels_last : {model_NHWC_params:,}")
+        # print(f"Channels_first: {model_NCHW_params:,}") 
+        self.assertEqual(model_NHWC_params, model_NCHW_params)
